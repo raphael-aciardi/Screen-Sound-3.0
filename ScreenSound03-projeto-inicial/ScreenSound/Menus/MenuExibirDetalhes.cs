c25e71f9fcdec22dd44c10b67c10130774652e9a
raphael-aciardi <996193248ph@gmail.com>
2024-07-03 21:45:35 -0300

    Criação da classe mãe Menu, criação da clase MenuAvaliarBanda e Herdando a classe Menu na classe criada e na classe MenuExibirDetalhes

--- ScreenSound03-projeto-inicial/ScreenSound/Menus/MenuExibirDetalhes.cs
+++ ScreenSound03-projeto-inicial/ScreenSound/Menus/MenuExibirDetalhes.cs
@@ -4,16 +4,9 @@ using ScreenSound.Modelos;
 
 namespace ScreenSound.Menus;
 
-internal class MenuExibirDetalhes
+internal class MenuExibirDetalhes : Menu
 {
-    void ExibirTituloDaOpcao(string titulo)
-    {
-        int quantidadeDeLetras = titulo.Length;
-        string asteriscos = string.Empty.PadLeft(quantidadeDeLetras, '*');
-        Console.WriteLine(asteriscos);
-        Console.WriteLine(titulo);
-        Console.WriteLine(asteriscos + "\n");
-    }
+
 
     public void Executar(Dictionary<string, Banda> bandasRegistradas)
     {
--- ScreenSound03-projeto-inicial/ScreenSound/Program.cs
+++ ScreenSound03-projeto-inicial/ScreenSound/Program.cs
@@ -57,11 +57,13 @@ internal class Program
                     MostrarBandasRegistradas();
                     break;
                 case 4:
-                    AvaliarUmaBanda();
+                    MenuAvaliarBanda menu4 = new();
+                    menu4.Execultar(bandasRegistradas);
+
                     break;
                 case 5:
-                    MenuExibirDetalhes menu = new();
-                    menu.Executar(bandasRegistradas);
+                    MenuExibirDetalhes menu5 = new();
+                    menu5.Executar(bandasRegistradas);
                     ExibirOpcoesDoMenu();
                     break;
                 case -1:
@@ -138,35 +140,6 @@ internal class Program
             Console.WriteLine(asteriscos);
             Console.WriteLine(titulo);
             Console.WriteLine(asteriscos + "\n");
-        }
-
-
-        void AvaliarUmaBanda()
-        {
-            Console.Clear();
-            ExibirTituloDaOpcao("Avaliar banda");
-            Console.Write("Digite o nome da banda que deseja avaliar: ");
-            string nomeDaBanda = Console.ReadLine()!;
-            if (bandasRegistradas.ContainsKey(nomeDaBanda))
-            {
-                Banda banda = bandasRegistradas[nomeDaBanda];
-                Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
-                Avaliacao nota = Avaliacao.Parse(Console.ReadLine()!);
-                banda.AdicionarNota(nota);
-                Console.WriteLine($"\nA nota {nota.Nota} foi registrada com sucesso para a banda {nomeDaBanda}");
-                Thread.Sleep(2000);
-                Console.Clear();
-                ExibirOpcoesDoMenu();
-            }
-            else
-            {
-                Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
-                Console.WriteLine("Digite uma tecla para voltar ao menu principal");
-                Console.ReadKey();
-                Console.Clear();
-                ExibirOpcoesDoMenu();
-            }
-
         }
 
         ExibirOpcoesDoMenu();
